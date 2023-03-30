51b2a6357acdc8a413d4e5fb1fc5f29536de692d
donggi-lee-bit <devdonggilee@gmail.com>
2023-03-30 17:15:21 +0900

    init: 초기 프로젝트 세팅
    - 필요할지 모르겠지만 spring web과 h2 db 의존성 추가
    - dependency
      - spring web
      - h2 database

---
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringApplication.class, args);
+    }
+
+}
